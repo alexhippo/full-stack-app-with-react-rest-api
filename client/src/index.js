70e76d874d2bbd0cd0e1bab4cfbae01266eca22d
Alex Hipolito <alexheepo@gmail.com>
2021-11-21 16:51:37 +1100

    Move order of CSS files, deleted unnecessary CSS files

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-
-import './styles/global.css';
 import './styles/reset.css';
+import './styles/global.css';
+
 
 ReactDOM.render(
   <BrowserRouter>
