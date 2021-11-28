5289ecb98b981c688354422270289cd47593b9ab
Alex Hipolito <alexheepo@gmail.com>
2021-11-28 14:47:08 +1100

    Redirect user after successfully signing in

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import Context from './Context';
 
 const PrivateRoute = () => {
   const context = useContext(Context.Context);
   const authUser = context.authenticatedUser;
+  const location = useLocation();
 
   // Use Outlet to render the child element (which would be Create Course or Update Course component) if there's an authUser
-  return authUser ? <Outlet /> : <Navigate to="/signin" />
+  return authUser ? <Outlet /> : <Navigate to="/signin" state={{ from: location.pathname }} />
 }
 
 export default PrivateRoute;
